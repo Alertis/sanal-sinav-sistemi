70a82e11cae4d229d886a4d3728bec4ca7ed934a
Alertis <osmayavuzdemir96@gmail.com>
2019-12-14 01:50:42 +0300

    Login sayfasının yeni tasarımı yapıldı

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { Card, Form, Icon, Input, Button } from 'antd';
 
 class Login extends Component {
    
     render() {
-
         return(
             <div className="loginPage">
-              Login Page
+              <Card className="wrapCard">
+                    <div>
+                        <img src="/assets/images/img-01.png" alt="IMG" />
+                    </div>
+                    <Form>
+                        <span className="title"> Sisteme Giriş</span>
+
+                        <Form.Item>
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Kullanıcı Adı" />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Şifre" />
+                        </Form.Item>
+                        
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Giriş Yap
+                            </Button>
+                        </Form.Item>
+                    </Form>
+              </Card>
             </div>
            
         );
@@ -14,3 +35,13 @@ class Login extends Component {
 } 
 
 export default Login;
+
+/**
+    <div class="wrap-login100">
+                    <div class="login100-pic js-tilt" data-tilt>
+                        <img src="images/img-01.png" alt="IMG" />
+                    </div>
+
+                   
+			    </div>
+**/
